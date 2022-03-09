857e51957b1101a733f0b7346c0cf23258af6647
IyasuH <eyasuhailegbr@gmail.com>
2022-03-09 07:03:00 -0800

    DBStorage - Review

--- models/place.py
+++ models/place.py
@@ -2,6 +2,10 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+from models.review import Review
+import os
+import models
 
 
 class Place(BaseModel, Base):
@@ -10,11 +14,25 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
+
+    if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+        @property
+        def reviews(self):
+            """
+            getter attribute that return the list of Review
+            instances with place_id equal to the current Place.id
+            """
+            list_reviews = []
+            for key, value in list(models.storage.all(Review)):
+                if value.place_id == self.id:
+                    list_reviews.append(value)
+            return list_reviews
