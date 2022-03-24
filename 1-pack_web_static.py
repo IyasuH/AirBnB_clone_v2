0af499e9f80b921e2b056fb3cf3e214f33b396d7
IyasuH <eyasuhailegbr@gmail.com>
2022-03-24 04:46:39 -0700

    updated for pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """
-A Fabric script that generates a .tgz archive from the contents of the web_static folder
+A Fabric script that generates a .tgz archive from the
+contents of the web_static folder
 """
 from fabric.api import local
 from fabric.api import execute
 import datetime
 import os
+
+
 def do_pack():
     """
     function to genrate .tgz file
     """
     now = datetime.datetime.now()
-    name = "web_static_" + str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute)
+    name = "web_static_" + str(now.year) + str(now.month) +
+    str(now.day) + str(now.hour) + str(now.minute)
     local("mkdir -p versions")
     local("tar -czvf versions/{}.tgz web_static".format(name))
     try:
