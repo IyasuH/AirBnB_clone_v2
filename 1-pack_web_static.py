0b0493792125e29c6f54813d3ae72923f4f51801
IyasuH <eyasuhailegbr@gmail.com>
2022-03-24 04:37:01 -0700

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A Fabric script that generates a .tgz archive from the contents of the web_static folder
+"""
+from fabric.api import local
+from fabric.api import execute
+import datetime
+import os
+def do_pack():
+    """
+    function to genrate .tgz file
+    """
+    now = datetime.datetime.now()
+    name = "web_static_" + str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute)
+    local("mkdir -p versions")
+    local("tar -czvf versions/{}.tgz web_static".format(name))
+    try:
+        f = open("versions/{}.tgz".format(name))
+        f.close()
+        path = os.path.relpath('versions/{}.tgz'.format(name), start=None)
+        file_size = os.stat('versions/{}.tgz'.format(name)).st_size
+        rt = "web_static packed: {} -> {}Bytes".format(path, file_size)
+        print(rt)
+        return(rt)
+    except IOError:
+        return(None)
