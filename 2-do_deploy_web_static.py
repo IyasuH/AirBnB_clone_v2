f0b7e01836f4e20e0ec30d503df2cb8a34952c83
IyasuH <eyasuhailegbr@gmail.com>
2022-03-24 11:14:01 -0700

    EDITED

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,11 @@
 Fabric script that distributes an archive to web server
 using the function do_deploy
 """
-from fabric.api import sudo, put, run, settings
+from fabric.api import sudo, put, run, settings, env
 import shutil
 import os
+
+env.hosts = ['34.139.86.96', '34.204.187.38']
 
 
 def do_deploy(archive_path):
@@ -13,21 +15,18 @@ def do_deploy(archive_path):
     deploy
     """
     if os.path.exists(archive_path):
-        print("file exist")
-        env.use_ssh_config = True
         try:
-            with settings(host_string=host.rstrip('\n').strip()):
-                put(archive_path, "/tmp/")
-                nm = archive_path.partition('/')[2]
-                nam = nm.partition('.')[0]
-                name = "/data/web_static/releases/{}".format(nam)
-                mn = "/tmp/{}".format(nm)
-                run("shutil.unpack_archive(name, mn)")
-                run("rm mn")
-                run("rm /data/web_static/current")
-                lin = "/data/web_static/releases/{}".format(nam)
-                run("ln -sf lin /data/web_static/current")
-                return True
+            put(archive_path, "/tmp/")
+            nm = archive_path.partition('/')[2]
+            nam = nm.partition('.')[0]
+            name = "/data/web_static/releases/{}".format(nam)
+            mn = "/tmp/{}".format(nm)
+            run("shutil.unpack_archive(name, mn)")
+            run("rm mn")
+            run("rm /data/web_static/current")
+            lin = "/data/web_static/releases/{}".format(nam)
+            run("ln -sf lin /data/web_static/current")
+            return True
         except Exception as e:
             return False
     else:
