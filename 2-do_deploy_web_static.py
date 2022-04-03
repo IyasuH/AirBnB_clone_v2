a8af13539701b3e5c17b9012d654a0555c113e42
IyasuH <eyasuhailegbr@gmail.com>
2022-04-03 03:59:30 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,17 +15,18 @@ def do_deploy(archive_path):
     deploy
     """
     if os.path.exists(archive_path):
+        path  = 'data/web_static/releases/'
+        noExt = archive_path.split('.')[0]
+        noPar = noExt.split('/')[1]
+        tru = path + noPar
         try:
-            put(archive_path, "/tmp/")
-            nm = archive_path.partition('/')[2]
-            nam = nm.partition('.')[0]
-            name = "/data/web_static/releases/{}".format(nam)
-            mn = "/tmp/{}".format(nm)
-            run("shutil.unpack_archive({}, {})".format(name, mn))
-            run("rm {}".format(mn))
-            run("rm /data/web_static/current")
-            lin = "/data/web_static/releases/{}".format(nam)
-            run("ln -sf {} /data/web_static/current".format(lin))
+            put(archive_path, "/tmp")
+            run("sudo mkdir -p {}".format(tru))
+            run("sudo tar -xzf /tmp/{}.tgz -C {}".format(noPar, tru))
+            run("sudo rm -f /tmp/{}.tgz".format(noPar))
+            run("sudo rm /data/web_static/current")
+            lin = "/data/web_static/releases/{}".format(noPar)
+            run("sudo ln -sf {} /data/web_static/current".format(lin))
             return True
         except Exception as e:
             return False
