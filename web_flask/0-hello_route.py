57ba8c9e6d6af88cf5f2fd060e055832c4ba6285
IyasuH <eyasuhailegbr@gmail.com>
2022-04-07 01:32:19 -0700

    following PEP8 style(v1.7)

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,13 @@ listening on 0.0.0.0 on port 5000
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-@app.route('/')
 
+
+@app.route('/')
 def hello_world():
     """ returns Hello HBNB! """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
