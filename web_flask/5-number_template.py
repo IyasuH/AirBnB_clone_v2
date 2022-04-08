492e666bf08c53e180a9c2b670060c7a116cd06c
IyasuH <eyasuhailegbr@gmail.com>
2022-04-07 19:53:30 -0700

    var defined

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -55,7 +55,7 @@ def number_di(n):
 @app.route('/number_template/<int:n>')
 def number_temp(n):
     """display HTML page only if n is an integer"""
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
