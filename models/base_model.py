26ed41ad54428f7ee3e4fde80a0753e1c52a1aeb
IyasuH <eyasuhailegbr@gmail.com>
2022-04-09 03:52:43 -0700

    upadet

--- models/base_model.py
+++ models/base_model.py
@@ -14,17 +14,20 @@ class BaseModel:
     updated_at = Column(DateTime, default=datetime.utcnow, nullable=False) 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        self.id =str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            kwargs['updated_at'] = datetime.strptime(self.updated_at.isoformat(),
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['created_at'] = datetime.strptime(self.created_at.isoformat(),
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            #del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
