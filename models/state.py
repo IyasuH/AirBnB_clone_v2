c8f59222518b981a39de137bd9352b9be0d02caf
IyasuH <eyasuhailegbr@gmail.com>
2022-04-09 09:07:44 -0700

    update

--- models/state.py
+++ models/state.py
@@ -23,7 +23,7 @@ class State(BaseModel, Base):
             instances with state_id equals to the current State.id
             """
             list_cities = []
-            for key, value in list(models.storage.all(City())):
-                if value.state_id == self.id:
-                    list_cities.append(value)
+            for key in list(models.storage.all(City).values()):
+                if key.state_id == self.id:
+                    list_cities.append(key)
             return list_cities
