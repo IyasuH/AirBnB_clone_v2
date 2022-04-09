f0bdf82b0bd9d52337ff1a038c51e12abc3dfb13
IyasuH <eyasuhailegbr@gmail.com>
2022-04-09 14:23:10 -0700

    task #9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+"""
+from flask import Flask, render_template
+from models import storage
+import models
+from models.state import State
+from sqlalchemy.orm import relationship
+from models.city import City
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """to remove current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states')
+def city_state():
+    """cities by states"""
+    state = storage.all(State)
+    return render_template('8-cities_by_states.html', state=state)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
